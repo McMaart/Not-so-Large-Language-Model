69905fbf2cb8cebb7661794be7db1078a5b0de37
Rikuma123 <rickmerweichenthal@gmail.com>
2024-04-16 02:27:45 +0200

    initial test gui cuda torch test (testtorch)

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -3,8 +3,34 @@ import gui
 
 class TestGui(unittest.TestCase):
     def test_gui_exists(self):
-        app = gui.GUI()
-        self.assertIsNotNone(app)  # add assertion here
+        self.assertIsNotNone(gui.GUI)  # add assertion here
+        self.assertIsNotNone(gui.Training)
+        self.assertIsNotNone(gui.Interaction)
+        #print(gui.GUI.children)
+
+class TestTraining(unittest.TestCase):
+    def test_start_training(self):
+        pass
+        #gui.Training.start_training()
+        #self.assertTrue(is_training)
+
+    def test_cancel_training(self):
+        pass
+        #gui.Training.cancel_training()
+        #self.assertFalse(is_training)
+
+class TestInteraction(unittest.TestCase):
+
+    def test_attributes(self):
+        #self.assertEqual(["Model 1", "Model 2", "Model 3"], )
+        #print(gui.Interaction.children)
+        pass
+
+    def test_submit_prompt(self):
+        pass
+
+    def evaluate(self):
+        pass
 
 
 if __name__ == '__main__':
