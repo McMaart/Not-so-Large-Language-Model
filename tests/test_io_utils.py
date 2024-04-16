b74072d868a2b4fda77156c784c003b41ffc43a8
Rikuma123 <rickmerweichenthal@gmail.com>
2024-04-16 03:13:56 +0200

    update test io utils

--- tests/test_io_utils.py
+++ tests/test_io_utils.py
@@ -9,6 +9,9 @@ class TestIO(unittest.TestCase):
         self.dummy_stories = ['This is a story.', 'This is also a story.', '"This is a quote in story 3".', 'test.']
         self.dummy_token_dict = {'this': 3, 'is': 3, 'a': 3, 'story': 3, '.': 4, 'also': 1, 'quote': 1, 'in': 1, '3': 1,
                                  'test': 1}
+        self.dummy_token_idx = {'this': 0, 'is': 1, 'a': 2, 'story': 3, '.': 4, 'also': 5, 'quote': 6, 'in': 7, '3': 8,
+                                'test': 9}
+
     def tearDown(self):
         pass
 
@@ -20,11 +23,26 @@ class TestIO(unittest.TestCase):
         self.assertListEqual(self.dummy_stories, loaded_stories)
         self.assertEqual(4, len(loaded_stories))
 
-    def test_get_vocabulary(self):
-        created_dict = io_utils.get_vocabulary(self.dummy_stories)
+    def test_get_vocabulary_frequencies(self):
+        created_dict = io_utils.get_vocabulary_frequencies(self.dummy_stories)
         self.assertEqual(10, len(created_dict))
         #print(io_utils.get_vocabulary(self.dummy_stories))
         self.assertDictEqual(self.dummy_token_dict, created_dict)
+
+    def test_get_vocabulary_frequencies_2(self):
+        created_dict = io_utils.get_vocabulary_frequencies_2(self.dummy_stories)
+        self.assertEqual(10, len(created_dict))
+        #print(io_utils.get_vocabulary_frequencies(self.dummy_stories))
+        self.assertDictEqual(self.dummy_token_dict, created_dict)
+
+    def test_get_vocabulary_idx(self):
+        self.assertDictEqual(self.dummy_token_idx, io_utils.get_vocabulary_idx(self.dummy_stories))
+
+    def test_map_story_to_tensor(self):
+        print(io_utils.map_story_to_tensor())
+
+    def test_tokens_to_story(self):
+        pass
 
     def test_save_to_file(self):
         test_file = io_utils.save_to_file("test.txt",['hi', 'hi2'])
