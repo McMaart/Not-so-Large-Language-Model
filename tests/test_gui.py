dfd78ab5640e6c3dbf132acdf662c4ffa62436d5
Rikuma123 <rickmerweichenthal@gmail.com>
2024-04-16 23:01:59 +0200

    test_gui components tests update

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,37 +1,67 @@
 import unittest
+from unittest.mock import patch, MagicMock
 import gui
 
-class TestGui(unittest.TestCase):
-    def test_gui_exists(self):
-        self.assertIsNotNone(gui.GUI)  # add assertion here
-        self.assertIsNotNone(gui.Training)
-        self.assertIsNotNone(gui.Interaction)
-        #print(gui.GUI.children)
+class TestGUIComponents(unittest.TestCase):
 
-class TestTraining(unittest.TestCase):
-    def test_start_training(self):
-        pass
-        #gui.Training.start_training()
-        #self.assertTrue(is_training)
 
-    def test_cancel_training(self):
-        pass
-        #gui.Training.cancel_training()
-        #self.assertFalse(is_training)
+    def setUp(self):
+        self.parent = MagicMock()
+        #self.controller = MagicMock()
+        controller = gui.GUI()
 
-class TestInteraction(unittest.TestCase):
+        self.training_instance = gui.Training(controller, controller)
+        self.interaction_instance = gui.Interaction(controller, controller)
 
-    def test_attributes(self):
-        #self.assertEqual(["Model 1", "Model 2", "Model 3"], )
-        #print(gui.Interaction.children)
-        pass
+    def test_gui_initialization(self):
+        controller = gui.GUI()
+        """Test GUI component is initialized correctly."""
+        self.assertIsNotNone(controller)
+        self.assertTrue(getattr(controller, "title"))
+        self.assertTrue(getattr(controller, "geometry"))
+        self.assertTrue(getattr(controller, "training"))
+        self.assertTrue(getattr(controller, "interaction"))
 
-    def test_submit_prompt(self):
-        pass
 
-    def evaluate(self):
-        pass
 
+    def test_training_initialization(self):
+        """Test Training component is initialized correctly."""
+        self.assertIsNotNone(self.training_instance)
+        self.assertTrue(getattr(self.training_instance, "controller"))
+        self.assertTrue(getattr(self.training_instance, "datasets"))
+        self.assertTrue(getattr(self.training_instance, "label"))
+
+        self.assertTrue(getattr(self.training_instance, "dataset_label"))
+
+        self.assertTrue(getattr(self.training_instance, "selection"))
+        self.assertTrue(getattr(self.training_instance, "dropdown"))
+
+        self.assertTrue(getattr(self.training_instance, "training_info"))
+
+        self.assertTrue(getattr(self.training_instance, "start_training"))
+        self.assertTrue(getattr(self.training_instance, "cancel_training"))
+
+    def test_interaction_initialization(self):
+        """Test Interaction component is initialized correctly."""
+        self.assertIsNotNone(self.interaction_instance)
+        self.assertTrue(getattr(self.interaction_instance, "controller"))
+
+        self.assertTrue(getattr(self.interaction_instance, "models"))
+
+        #self.assertTrue(getattr(self.interaction_instance, "history"))
+
+        self.assertTrue(getattr(self.interaction_instance, "model_label"))
+        self.assertTrue(getattr(self.interaction_instance, "model_selection"))
+        self.assertTrue(getattr(self.interaction_instance, "dropdown"))
+
+        self.assertTrue(getattr(self.interaction_instance, "chat"))
+
+        self.assertTrue(getattr(self.interaction_instance, "entry"))
+
+        self.assertTrue(getattr(self.interaction_instance, "send"))
+
+        self.assertTrue(getattr(self.interaction_instance, "eval_label"))
+        self.assertTrue(getattr(self.interaction_instance, "eval"))
 
 if __name__ == '__main__':
     unittest.main()
