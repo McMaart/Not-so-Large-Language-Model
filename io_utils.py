82acc45f03b73aef3764a1d9b26a25fd4dc183e7
Maarten <maarten.maecking@web.de>
2024-05-11 17:20:32 +0200

    Add DataLoader class for TinyStories

--- io_utils.py
+++ io_utils.py
@@ -1,12 +1,15 @@
 import pickle
+import time
 import torch
 from datasets import load_dataset
+from torch.nn.utils.rnn import pad_sequence
 from torchtext.data import get_tokenizer
 from torch import Tensor
 import nltk
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 import re
 from model_1 import TransformerModel, device
+from torch.utils.data import Dataset
 
 
 def load_tiny_stories(end: int, start: int = 0, split: str = "train") -> list[str]:
@@ -19,7 +22,7 @@ def load_tiny_stories(end: int, start: int = 0, split: str = "train") -> list[st
     :param split: Choice between 'train' or 'validation' split
     :return: List of stories
     """
-    return load_dataset("roneneldan/TinyStories")[split][start:end]['text']
+    return load_dataset("roneneldan/TinyStories", split=split)[start:end]['text']
 
 
 def load_from_file(filename: str) -> list[str]:
@@ -54,7 +57,7 @@ def get_vocabulary_frequencies(story_list: list[str]) -> dict[str, int]:
     return vocabulary
 
 
-def get_vocabulary_idx(story_list: list, max_words: int | None = None) -> dict[str, int]:
+def get_vocabulary_idx(story_list: list[str], max_words: int | None = None) -> dict[str, int]:
     """
     Assigns an index to each word that appears in the list of stories
     """
@@ -66,6 +69,7 @@ def get_vocabulary_idx(story_list: list, max_words: int | None = None) -> dict[s
         vocab_freq = vocab
 
     vocab_freq['<unk>'] = 0  # Placeholder for tokens that do not appear in the story
+    vocab_freq['<pad>'] = 0  # Pad token for batching
     return {k: idx for idx, k in enumerate(vocab_freq.keys())}
 
 
@@ -84,8 +88,11 @@ def clean_stories(story_list: list[str]) -> list[str]:
     """
     for idx, story in enumerate(story_list):
         if 'â' in story:
-            story = story.replace('â€œ', '"')
             story = story.replace('â€™', "'")
+            story = story.replace("â€“", "-")
+            story = story.replace("â€”", " - ")
+            story = story.replace("â€š", ",")
+            story = story.replace("â€œ", '"')
             story = story.replace('â€', '"')
             story_list[idx] = story
     return [story for story in story_list if story.isascii() and len(story) > 1]
@@ -128,23 +135,53 @@ def prompt_model(model_name: str, start_token: str, length: int = 50) -> str:
     return tokens_to_story(token_list)
 
 
+class TinyStories(Dataset):
+    def __init__(self, vocabulary, tokenizer=get_tokenizer('basic_english'), split: str = "train", ):
+        self.stories = load_dataset("roneneldan/TinyStories", num_proc=4, split=split)
+        self.default_token = vocab["<unk>"]
+        self.vocab = vocabulary
+        self.tokenizer = tokenizer
+
+    def __getitem__(self, index):
+        story = self.stories[index]['text']
+        story_list = clean_stories([story])
+        return torch.tensor([self.vocab.get(word, self.default_token) for word in self.tokenizer(story_list[0])],
+                            dtype=torch.int64) if len(story_list) else torch.tensor([], dtype=torch.int64)
+
+    def __len__(self):
+        return len(self.stories)
+
+
 def save_vocabulary(vocab: dict[str, int], filename: str = "trained_models/vocabulary.pkl"):
-    with open(filename, 'wb') as f:
-        pickle.dump(vocab, f)
+    with open(filename, 'wb') as file:
+        pickle.dump(vocab, file)
 
 
 def load_vocabulary(filename: str = "trained_models/vocabulary.pkl") -> dict:
-    with open(filename, 'rb') as f:
-        return pickle.load(f)
+    with open(filename, 'rb') as file:
+        return pickle.load(file)
 
 
 if __name__ == "__main__":
-    stories = load_from_file("data/100stories.txt")
-    stories = clean_stories(stories)
-    # save_to_file("data/100stories.txt", stories)
-    print("Number of stories:", len(stories))
+    from torch.utils.data import DataLoader
 
-    token_dict = get_vocabulary_frequencies(stories)
-    token_dict_sorted = {k: v for k, v in sorted(token_dict.items(), key=lambda item: item[1], reverse=True)}
-    print(f"Number of tokens: {len(token_dict)}")
-    print("Token frequency:", token_dict_sorted)
+    vocab = load_vocabulary()
+    data = TinyStories(vocab)
+    # ToDo: make sure pad_sequence pads with pad_token
+    dataloader = DataLoader(data, batch_size=32, collate_fn=pad_sequence, num_workers=8)
+
+    i = 0
+    for data in dataloader:
+        print(data.shape)
+        i += 1
+        if i == 40:
+            break
+
+    # stories = load_tiny_stories(1000)
+    # stories = clean_stories(stories)
+    # print("Number of stories:", len(stories))
+    #
+    # token_dict = get_vocabulary_frequencies(stories)
+    # token_dict_sorted = {k: v for k, v in sorted(token_dict.items(), key=lambda item: item[1], reverse=True)}
+    # print(f"Number of tokens: {len(token_dict)}")
+    # print("Token frequency:", token_dict_sorted)
