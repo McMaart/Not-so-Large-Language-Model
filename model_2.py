72b13b8873ead33f64fdca6a64d8656ae162203e
Maarten <maarten.maecking@web.de>
2024-06-12 07:02:11 +0200

    Add LSTM and GRU models

--- model_2.py
+++ model_2.py
@@ -1,29 +1,12 @@
-"""  
-RNN reference model 
+"""
+RNN reference model
 """
 import torch
 from torch import nn, Tensor
-import torch.nn.functional as F
-from model_1 import num_special_non_eos_tokens
-
-device = (
-    "cuda" if torch.cuda.is_available()
-    else "mps" if torch.backends.mps.is_available()
-    else "cpu"
-)
-learning_rate_rnn = 8e-4
-batch_size_rnn = 32
-max_seq_len_rnn = 100
-embed_size_rnn = 256
-hidden_size_rnn = 128
-num_layers_rnn = 2
-dropout_rnn = 0.2
-dropout_rnn_2 = 0.2
 
 
-class RNNModel(nn.Module):
-    def __init__(self, vocab_size: int, embed_size: int = 256, hidden_size: int = 256, num_layers: int = 2,
-                 dropout_rnn: float = 0.1, dropout_2: float = 0.1, padding_idx: int | None = None):
+class RNNBaseModel(nn.Module):
+    def __init__(self, vocab_size: int, embed_size: int, hidden_size: int, num_layers: int, padding_idx: int | None):
         super().__init__()
         self.vocab_size = vocab_size
         self.embed_size = embed_size
@@ -31,25 +14,52 @@ class RNNModel(nn.Module):
         self.num_layers = num_layers
 
         self.embedding = nn.Embedding(self.vocab_size, self.embed_size, padding_idx=padding_idx)
-        self.rnn = nn.RNN(self.embed_size, self.hidden_size, self.num_layers, batch_first=True, dropout=dropout_rnn)
-        self.dropout = nn.Dropout(dropout_2)
         self.linear = nn.Linear(self.hidden_size, self.vocab_size)
-
-    def forward(self, x: Tensor, h: Tensor) -> tuple[Tensor, Tensor]:
-        if h is None:
-            if x.dim() == 2:  # Batched input
-                h = self.init_hidden(x.size(0))
-            else:  # Unbatched input
-                h = torch.zeros(self.num_layers, self.hidden_size, device=next(self.parameters()).device)
-        x = self.embedding(x)
-        out, h = self.rnn(x, h)
-        out = self.dropout(out)
-        out = self.linear(out)
-        return out, h
 
     def init_hidden(self, batch_size: int) -> Tensor:
         return torch.zeros(self.num_layers, batch_size, self.hidden_size, device=next(self.parameters()).device,
                            requires_grad=False)
+
+
+class RNNModel(RNNBaseModel):
+    def __init__(self, vocab_size: int, embed_size: int = 256, hidden_size: int = 384, num_layers: int = 6,
+                 dropout: float = 0.1, padding_idx: int | None = None, dropout2: float = 0.1):
+        super().__init__(vocab_size, embed_size, hidden_size, num_layers, padding_idx)
+        self.rnn = nn.RNN(self.embed_size, self.hidden_size, self.num_layers, batch_first=True, dropout=dropout)
+        self.dropout = nn.Dropout(dropout2)
+
+    def forward(self, x: Tensor):
+        h = self.init_hidden(x.size(0))
+        embedding = self.embedding(x)
+        out, _ = self.rnn(embedding, h)
+        out = self.dropout(out)
+        return self.linear(out)
+
+
+class GRUModel(RNNBaseModel):
+    def __init__(self, vocab_size: int, embed_size: int = 256, hidden_size: int = 256, num_layers: int = 4,
+                 dropout: float = 0.1, padding_idx: int | None = None):
+        super().__init__(vocab_size, embed_size, hidden_size, num_layers, padding_idx)
+        self.gru = nn.GRU(self.embed_size, self.hidden_size, self.num_layers, batch_first=True, dropout=dropout)
+
+    def forward(self, x: Tensor):
+        h = self.init_hidden(x.size(0))
+        embedding = self.embedding(x)
+        out, _ = self.gru(embedding, h)
+        return self.linear(out)
+
+
+class LSTMModel(RNNBaseModel):
+    def __init__(self, vocab_size: int, embed_size: int = 256, hidden_size: int = 256, num_layers: int = 4,
+                 dropout_lstm: float = 0.2, padding_idx: int | None = None):
+        super().__init__(vocab_size, embed_size, hidden_size, num_layers, padding_idx)
+        self.lstm = nn.LSTM(self.embed_size, self.hidden_size, self.num_layers, batch_first=True, dropout=dropout_lstm)
+
+    def forward(self, x: Tensor):
+        h, c = self.init_hidden(x.size(0)), self.init_hidden(x.size(0))
+        embedding = self.embedding(x)
+        out, _ = self.lstm(embedding, (h, c))
+        return self.linear(out)
 
 
 if __name__ == "__main__":
@@ -71,7 +81,6 @@ if __name__ == "__main__":
     #     print()
     
     # actual_stories = get_stories(prompt, 10000)
-
     # print(len(stories))
     # print(len(actual_stories))
 
