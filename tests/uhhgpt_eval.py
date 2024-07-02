25e3612f36399193f92486962f45c795d995ab82
Maarten <maarten.maecking@web.de>
2024-07-02 19:12:00 +0200

    UHHGTP-Eval: Improved handling of multi-line prompts, close #20

--- tests/uhhgpt_eval.py
+++ tests/uhhgpt_eval.py
@@ -15,11 +15,7 @@ from os import environ
 from selenium.common import NoSuchElementException
 from selenium.webdriver import Chrome, Keys
 from selenium.webdriver.common.by import By
-
-uhhgpt_website = {'login_button': '#inhalt > main > section.spalten > article > div:nth-child(2) > div > div > '
-                                  'div.loginform > div.left > form > div.form-element-wrapper > button',
-                  'pass': '#password',
-                  'user': '#username'}
+from prompt_testing.parse_model_reply import parse_prompt_0
 
 
 class ChromeDriver(Chrome):
@@ -28,12 +24,11 @@ class ChromeDriver(Chrome):
         self.maximize_window()
         self.implicitly_wait(wait)
 
-    def click_css_element(self, selector):
-        elem = self.find_element(By.CSS_SELECTOR, selector)
-        elem.click()
+    def click_css_element(self, selector: str):
+        self.find_element(By.CSS_SELECTOR, selector).click()
 
 
-def get_response(driver: ChromeDriver, wait=3, max_iter=100):
+def get_response(driver: ChromeDriver, wait: int | float = 3, max_iter: int = 100):
     prev_response = "NAN"
     for _ in range(max_iter):
         time.sleep(wait)
@@ -45,31 +40,82 @@ def get_response(driver: ChromeDriver, wait=3, max_iter=100):
         prev_response = cur_response
 
 
-# Login
-driver = ChromeDriver()
-driver.get("https://uhhgpt.uni-hamburg.de/login.php")
-driver.click_css_element('body > div > aside > div.loginPanel > form > button')
-try:
-    user_elem = driver.find_element(By.CSS_SELECTOR, '#username')
-except NoSuchElementException as exc:
-    print(exc, file=sys.stderr)
-    sys.exit(1)
+def setup() -> ChromeDriver:
+    # Login
+    driver = ChromeDriver()
+    driver.get("https://uhhgpt.uni-hamburg.de/login.php")
+    driver.click_css_element('body > div > aside > div.loginPanel > form > button')  # login button on title page
+    try:
+        user_elem = driver.find_element(By.CSS_SELECTOR, '#username')
+    except NoSuchElementException as exc:
+        print(exc, file=sys.stderr)
+        sys.exit(1)
 
-user_elem.send_keys(environ.get('PASS_UNI')[:7])
-pass_elem = driver.find_element(By.CSS_SELECTOR, uhhgpt_website.get('pass'))
-pass_elem.send_keys(environ.get('PASS_UNI')[7:])
-driver.find_element(By.CSS_SELECTOR, uhhgpt_website.get('login_button')).click()
+    user_elem.send_keys(environ.get('PASS_UNI')[:7])
+    pass_elem = driver.find_element(By.CSS_SELECTOR, '#password')
+    pass_elem.send_keys(environ.get('PASS_UNI')[7:])
+    driver.click_css_element('#inhalt > main > section.spalten > article > div:nth-child(2) > div > div > '
+                             'div.loginform > div.left > form > div.form-element-wrapper > button')  # confirm login
+    driver.click_css_element('#data-protection > div > button')  # accept terms of use
 
-# Change to GPT-4o
-driver.click_css_element('#data-protection > div > button')
-driver.click_css_element('body > div > div.sidebar > div.menu > div.radiogroup > label:nth-child(2) > p')
-new_chat = driver.find_element(By.CSS_SELECTOR, 'body > div > div.main > div.input-container > center > button')
-new_chat.click()
+    # Change to GPT-4o model
+    # choose the GPT-4o-model from the menu
+    driver.click_css_element('body > div > div.sidebar > div.menu > div.radiogroup > label:nth-child(2) > p')
+    driver.click_css_element('body > div > div.main > div.input-container > center > button')  # start new chat
+    return driver
 
-# Prompt to the model
-sample_prompt = "Hello, is the university of Hamburg (UHH) excellent?"
-prompt_elem = driver.find_element(By.CSS_SELECTOR, '#texreachat')
-prompt_elem.send_keys(f"{sample_prompt}?{Keys.RETURN}")
-answer = get_response(driver)
-print(f"Chat_GPT: {answer}")
-time.sleep(1200)
+
+def get_ratings(driver: ChromeDriver, instruction: str, generated_stories: list[str]) -> list[list[int | None]]:
+    story_ratings = []
+    for story in generated_stories:
+        full_prompt = instruction.format(story)
+        prompt_elem = driver.find_element(By.CSS_SELECTOR, '#texreachat')
+        driver.execute_script("arguments[0].value = arguments[1];", prompt_elem, full_prompt)
+        prompt_elem.send_keys(Keys.RETURN)
+
+        answer = get_response(driver)
+        story_ratings.append(parse_prompt_0(answer))
+
+        driver.click_css_element('body > div > div.main > div.input-container > center > button')  # new chat
+        print(f"UHHGPT:\n{answer}", end="\n\n")
+
+    return story_ratings
+
+
+if __name__ == '__main__':
+    # ToDo: Import (/replace with) the best prompt
+    sample_prompt = """
+    The following story is a story written for children at the age of around 5 years old.
+    Your task is to rate the story objectively.
+
+    Story:
+    {}
+
+    Please rate the story in the following categories:
+    - GRAMMAR (exclusively rate the grammar of the story)
+    - SPELLING (exclusively rate the spelling of the story)
+    - CONSISTENCY (exclusively rate the consistency of the story)
+    - STORY (exclusively rate the story/plot of the story)
+    - CREATIVITY (exclusively rate the creativity of the story)
+    - STYLE (exclusively rate the linguistic style of the story)
+
+    Rate objectively and rate each category independently from the others.
+    Rate each category with a score from 1 to 10, with 1 being the worst and 10 being the best.
+
+    It is crucial that your response ends with the following format (substitute <YOUR_CATEGORY_SCORE> with the score you gave for the respective category) and does not include any other text afterwords:
+
+    GRAMMAR: <YOUR_GRAMMAR_SCORE>
+    SPELLING: <YOUR_SPELLING_SCORE>
+    CONSISTENCY: <YOUR_CONSISTENCY_SCORE>
+    STORY: <YOUR_STORY_SCORE>
+    CREATIVITY: <YOUR_CREATIVITY_SCORE>
+    STYLE: <YOUR_STYLE_SCORE>
+    """
+    sample_story = """
+    Once upon a time, there was a little girl named Lily. She had a big, red ball that she loved to play with. One day, she saw a small, helpless bird on the ground. The bird could not fly. Lily wanted to help the bird. Lily said to the bird, "Don't worry, little bird. I will help you." She picked up the bird and took it to her mom. Her mom knew how to help the bird. Her mom said, "We can give the bird some food and water." Lily and her mom gave the bird some food and water. The bird started to feel better. It was not helpless anymore. The bird said, "Thank you, Lily and mom." Lily was happy to help the bird. She learned that helping others can make you feel good too. And from that day on, Lily always tried to help others when they needed it.
+    """
+    stories = [sample_story, sample_story, sample_story]
+
+    driver = setup()
+    ratings = get_ratings(driver, sample_prompt, stories)
+    print(f"Rating list: {ratings}")
